92073a58d6af5d1d1bd21f68a910ca485433d08f
joemulberry <emailmeit@protonmail.com>
2021-08-18 12:16:09 +0100

    ready to test

--- last50.py
+++ last50.py
@@ -2,40 +2,16 @@ import requests
 import json 
 from time import time
 
-''''
-dict_keys(['approved_account', 
-'asset', 'asset_bundle', 
-'auction_type', 'bid_amount', 
-'collection_slug', 
-'contract_address', 
-'created_date', 
-'custom_event_name', 
-'dev_fee_payment_event', 
-'duration', 
-'ending_price', 
-'event_type', 
-'from_account', 
-'id', 
-'owner_account', 
-'payment_token', 
-'quantity',
-'seller', 
-'starting_price', 
-'to_account', 
-'total_price', 
-'transaction', 
-'winner_account'])
-'''
-
 url = "https://api.opensea.io/api/v1/events"
 querystring = {"asset_contract_address":"0x76be3b62873462d2142405439777e971754e8e77","event_type":"successful","only_opensea":"false","offset":"0","limit":"50"}
 headers = {"Accept": "application/json"}
 response = requests.request("GET", url, headers=headers, params=querystring)
-
 data = json.loads(response.text)
 events = data['asset_events']
 
-print(events[0].keys())
+url = 'https://raw.githubusercontent.com/joemulberry/last_50/main/last50.json'
+resp = requests.get(url)
+last50_data = json.loads(resp.text)
 
 eth = 0
 not_eth = 0
@@ -58,39 +34,12 @@ di = {
     'a': eth,
     'b': not_eth,
     't': time()
-}
+    }
 
-print(di)
+last50_data.append(di)
 
-# for d in os_data:
+with open('last50.json', 'w') as fout:
+    json.dump(last50_data, fout)
 
-#     if d['last_sale'] != None:
-#         if d['last_sale']['payment_token']['symbol'] == 'ETH':
-#             eth += 1
-#         else:
-#             not_eth += 1
-
-# total_count = not_eth+eth
-# print((eth, not_eth, total_count))
-
-# eth = eth / total_count
-# not_eth = not_eth / total_count
-
-# eth = round(eth, 2)
-# not_eth = round(not_eth, 2)
-
-# di = {
-#     'a': eth,
-#     'b': not_eth,
-#     't': time()
-# }
-
-# w_or_e_data.append(di)
-
-# print(w_or_e_data)
-
-# with open('ask_bids.json', 'w') as fout:
-#     json.dump(w_or_e_data, fout)
-
-# with open('ask_bids_now.json', 'w') as fout:
-#     json.dump([di], fout)
+with open('last50_now.json', 'w') as fout:
+    json.dump([di], fout)
